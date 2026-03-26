0b8372fd40b9e4317e7046812c9fd66913d674ea
FelipeMartinezCaceres <felipe.martinez@uc.cl>
2026-03-26 11:14:24 -0300

    print eleccion

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 
 print("Hola, bienvenido al juego de ´piedra, papel, tijeras, lagarto o Spock´")
-
-usuario = input("Elige entre estas opciones: ")
+print("Elige entre estas opciones colocando su numero: \n1: Spock \n2: tijeras \n3: papel \n4: roca \n5: lagarto")
+usuario = int(input())
 spock = "spock"
 tijeras = "tijeras"
 papel = "papel"
