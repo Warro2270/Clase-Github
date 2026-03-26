748c153e6147442bbf544778bd39b97663d5ba51
diego <diego.barrios@uc.cl>
2026-03-26 11:09:06 -0300

    varibles a numeros

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 print("Hola, bienvenido al juego de ´piedra, papel, tijeras, lagarto o Spock´")
 
 usuario = input("Elige entre estas opciones: ")
-spock = "spock"
-tijeras = "tijeras"
-papel = "papel"
-roca = "roca"
-lagarto = "lagarto"
+spock = 1
+tijeras = 2
+papel = 3
+roca = 4
+lagarto = 5
