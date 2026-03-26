439913f901476f14e7c792e0f26978739c873d28
diego <diego.barrios@uc.cl>
2026-03-26 11:16:45 -0300

    variables a numero con nuevo cambio

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 print("Hola, bienvenido al juego de ´piedra, papel, tijeras, lagarto o Spock´")
 print("Elige entre estas opciones colocando su numero: \n1: Spock \n2: tijeras \n3: papel \n4: roca \n5: lagarto")
 usuario = int(input())
-spock = "spock"
-tijeras = "tijeras"
-papel = "papel"
-roca = "roca"
-lagarto = "lagarto"
+spock = 1
+tijeras = 2
+papel = 3
+roca = 4
+lagarto = 5
